51c05f5885659259e21c0eec95bf18758b7ff09a
mohsin <mohsin@gnowit.com>
2022-10-19 21:00:54 -0400

    con

--- Backend/services/mongooseConnection.js
+++ Backend/services/mongooseConnection.js
@@ -4,11 +4,28 @@ const mongoose = require('mongoose'),
 
 exports.makeConnection = async () => {
 
-    let [err, con] = await utils.resolver(mongoose.connect('mongodb://localhost:27017/DataIntreaction', { useNewUrlParser: true }));
-    if (!err)
-        return console.log("can not connect to db")
-    console.log("Connected to DB")
-    return con
+    let db = "DataIntreaction"
+    if (NODE.ENV === 'production') {
+        let [err, con] = await utils.resolver(mongoose.connect('mongodb://localhost:27017/' + db, {
+            useNewUrlParser: true, useFindAndModify: false, "auth": {
+                "authSource": "admin"
+            },
+            "user": "admin",
+            "pass": "VWg4Q1gzjNV6FF1oPqwxERq6GEfIwQrDpAtkCE7s3hUDmZO7yvgqzI"
+        }))
+        if (!err)
+            return console.log("can not connect to db")
+        console.log("Connected to DB")
+        return con
+    }
 
+    else {
+
+        let [err, con] = await utils.resolver(mongoose.connect('mongodb://localhost:27017/' + db, { useNewUrlParser: true }));
+        if (!err)
+            return console.log("can not connect to db")
+        console.log("Connected to DB")
+        return con
+    }
 
 }
