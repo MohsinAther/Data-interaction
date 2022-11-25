2b8a0a16c20820cfe8ea7ab72187daae9b0da118
mohsin <mohsin@gnowit.com>
2022-11-25 17:06:27 -0500

    con err

--- Backend/services/mongooseConnection.js
+++ Backend/services/mongooseConnection.js
@@ -16,7 +16,7 @@ exports.makeConnection = async () => {
             "pass": "VWg4Q1gzjNV6FF1oPqwxERq6GEfIwQrDpAtkCE7s3hUDmZO7yvgqzI"
         }))
         if (err)
-            return console.log("Can not connected to DB, prod")
+            return console.log("Can not connected to DB, prod", err)
         console.log("Can connected to db, prod")
         return con
     }
