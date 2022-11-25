0e034efe0679ad567f45c752e750f51faf800131
mohsin <mohsin@gnowit.com>
2022-11-25 16:49:40 -0500

    correct data

--- Backend/services/seed.js
+++ Backend/services/seed.js
@@ -5,32 +5,32 @@ let mongoose = require('mongoose'),
 exports.seedCompanies = async (req, res) => {
     //comapniesList = require('../data/YahooUTF8.json');
     comapniesList = [{
-        "OEDV": "AAPL",
-        "Osage Exploration and Development, Inc.": "Apple Inc.",
+        "ticker": "AAPL",
+        "company": "Apple Inc.",
         "PNK": "NMS",
         "FIELD4": "Electronic Equipment",
         "USA": "USA",
         "FIELD6": ""
     },
     {
-        "OEDV": "BAC",
-        "Osage Exploration and Development, Inc.": "Bank of America Corporation",
+        "ticker": "BAC",
+        "company": "Bank of America Corporation",
         "PNK": "NYQ",
         "FIELD4": "Money Center Banks",
         "USA": "USA",
         "FIELD6": ""
     },
     {
-        "OEDV": "AMZN",
-        "Osage Exploration and Development, Inc.": "Amazon.com, Inc.",
+        "ticker": "AMZN",
+        "company": "Amazon.com, Inc.",
         "PNK": "NMS",
         "FIELD4": "Catalog & Mail Order Houses",
         "USA": "USA",
         "FIELD6": ""
     },
     {
-        "OEDV": "T",
-        "Osage Exploration and Development, Inc.": "AT&T Inc.",
+        "ticker": "T",
+        "company": "AT&T Inc.",
         "PNK": "NYQ",
         "FIELD4": "Telecom Services - Domestic",
         "USA": "USA",
